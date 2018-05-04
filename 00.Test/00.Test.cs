1c8c946e3fd359661ef1550588e4dc43b1d87369
Biser Damyanovski <biserdam@gmail.com>
2018-05-04 21:28:24 +0300

    Added Lecture 4 Solution

---
+++ 00.Test/00.Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var namesWithNumbers = new Dictionary<string, string>();
+            namesWithNumbers.Add("John", "+359-878-000-999");
+            namesWithNumbers["Pesho"] = "1234";
+
+        }
+    }
+}
